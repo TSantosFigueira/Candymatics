afe11a885944066f39841b664e120133d9dbc422
TSantosFigueira <tsantosfigueira@outlook.com>
2016-05-22 15:12:30 -0300

    Adicionada tela de troca de personagens; Código para construção de personagens em construção.

---
+++ Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelection : MonoBehaviour {
+
+    void maleCharacterSelected()
+    {
+        PlayerPrefs.SetString("character", "male");
+    }
+
+    void femaleCharacterSelected()
+    {
+        PlayerPrefs.SetString("character", "female");
+    }
+}
